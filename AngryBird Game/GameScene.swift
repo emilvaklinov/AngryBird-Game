a1c69ece53d96c140ce6a9d11f0cf0016322a5f7
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-06-19 15:52:14 +0100

    moving the bird

--- AngryBird Game/GameScene.swift
+++ AngryBird Game/GameScene.swift
@@ -20,6 +20,8 @@ class GameScene: SKScene {
     var box4 = SKSpriteNode()
     var box5 = SKSpriteNode()
     
+    var gameStarted = false
+    
     override func didMove(to view: SKView) {
 //        let texture = SKTexture(imageNamed: "bird")
 //        bird2 = SKSpriteNode(texture: texture)
@@ -99,9 +101,27 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        bird.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 100))
-        bird.physicsBody?.affectedByGravity = true
+//        bird.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 100))
+//        bird.physicsBody?.affectedByGravity = true
         
+        if gameStarted == false {
+            
+            if let touch = touches.first {
+                let touchLocation = touch.location(in: self)
+                let touchNodes = nodes(at: touchLocation)
+                
+                if touchNodes.isEmpty == false {
+                    
+                    for node in touchNodes {
+                        if let sprite = node as? SKSpriteNode {
+                            if sprite == bird {
+                                bird.position = touchLocation
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
